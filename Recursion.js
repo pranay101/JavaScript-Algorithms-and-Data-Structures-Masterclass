491bf2f493567aedc1549b16b55ec195fcd5a41e
pranay101 <pranayprajapati101@gmail.com>
2021-08-12 11:36:46 +0530

    done with helper method recursion and pure recursion

--- Recursion.js
+++ Recursion.js
@@ -1,6 +1,10 @@
 
 // ------------------------------------------------ Functions ----------------------------------------------------------------
 
+
+
+
+
 // -------Factorial--------
 
 function factorial(number)
@@ -9,7 +13,7 @@ function factorial(number)
     return number* factorial(number-1)
 }
 
-// ------------------------
+
 
 
 // ---------sumRange-------
@@ -19,13 +23,57 @@ function sumRange(number)
     if (number === 1) return 1
     return number + sumRange(--number)
 }
-// ------------------------
 
 
 
+
+// -------------------------------------------Helper Method recursion------------------------------------------
+
+
+// ----------------collectOddvalues---------------
+
+function helper_method_collectOddvalues(arr)
+{
+    let newrr = []
+    function helper(input_array)
+    {
+        if(input_array.length === 0)
+            return;
+
+        if(!(input_array[0] % 2 === 0))
+            newrr.push(input_array[0])
+        
+        helper(input_array.slice(1))
+    }
+    helper(arr)
+    return newrr
+}
+
+// ------------------------------------------------
+
+// -------------------------------------------Pure recursion------------------------------------------
+
+
+// ----------------collectOddvalues---------------
+
+function collectOddvalues(arr)
+{
+    let newarr = []
+    if( arr.length === 0)
+     return newarr
+     
+    if(arr[0] % 2 !== 0)
+        newarr.push(arr[0])
+
+    newarr = newarr.concat(collectOddvalues(arr.slice(1)))
+    return newarr
+}
+
+// ------------------------------------------------
 
 
 
 // ----------------------------------------------------------------------------------------------------------------
 // console.log(factorial(5))
-console.log(sumRange(5))
+// console.log(sumRange(5))
+console.log(collectOddvalues([1,2,3,4,5,6,7,8,9]))
