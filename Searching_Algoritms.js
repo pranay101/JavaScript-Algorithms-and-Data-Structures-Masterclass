4cea36c9bb6cc5a35eb5da14fc3546f850e92576
pranay101 <pranayprajapati101@gmail.com>
2021-08-12 18:22:58 +0530

    done with binary search

--- Searching_Algoritms.js
+++ Searching_Algoritms.js
@@ -10,8 +10,41 @@ function linear_search(arr,element)
     return -1
 }
 
+// Similar function is javaScript that uses linear linear_search
+// find()
+// indexof()
+// findindex()
+// includes()
+// --------------------------------------------------------------------------------------------------------------
 
 
 
-console.log(linear_search([8,6,3,2,6,8,3],2))
-console.log(linear_search([8,6,3,2,6,8,3],1))
+// ---------------------------------------------Binary Searching-------------------------------------------------
+
+function binary_search(arr,element)
+{
+    let high = arr.length-1;
+    let low = 0
+    let middle ;
+    
+    while (low < high)
+    {
+        middle = Math.ceil((low+high)/2);
+        if(arr[middle] === element)
+            return middle
+        if(arr[middle] > element)
+            high =  middle
+        else(arr[middle] < element)
+            low = middle
+    }
+    return -1
+}
+
+// --------------------------------------------------------------------------------------------------------------
+
+
+// console.log(linear_search([8,6,3,2,6,8,3],2))
+// console.log(linear_search([8,6,3,2,6,8,3],1))
+
+console.log(binary_search([2,3,5,6,7,8,18],8))
+console.log(binary_search([2,3,5,6,7,8,18],4))
