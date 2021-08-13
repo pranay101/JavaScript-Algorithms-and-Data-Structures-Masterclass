d276edb7b91aa76fb60db162a82a141a16549d29
pranay101 <pranayprajapati101@gmail.com>
2021-08-13 20:44:30 +0530

    fixed typos

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,16 +1,14 @@
 function bubbleSort(array){
+    console.log(array)
     for (let index = 0; index < array.length; index++) {
         for (let jndex = index+1; jndex < array.length; jndex++) {
-            if (array[index] > array[jndex]) {
-                let temp = array[index]
-                array[index] = array[jndex]
-                array[jndex] = temp
-            }
-            
+            if (array[index] > array[jndex]) 
+                [array[index],array[jndex]] = [array[jndex],array[index]]          
         }
     }
+    console.log(array)
     return array
 }
 
 
-console.log(bubbleSort([9,6565443,4,3,786755,7,8,4,3,6,7,8,9,9,5,4,3212,4536,123,3,3,3,4555,6,788,5,]))
+console.log(bubbleSort([11,10,9,8,7,6,5,4,3,2,1,0]))
