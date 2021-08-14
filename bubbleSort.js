6b1f573c932080d9ff18e1e082220663511ec72c
pranay101 <pranayprajapati101@gmail.com>
2021-08-14 10:11:01 +0530

    optimised naive bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -16,14 +16,15 @@ function naivebubbleSort(array){
 // -------------------------------------------Naive Bubble sort 2------------------------------------------
 function bubbleSort(array){
     console.log(array)
-    for (let index = 0; index < array.length; index++) {
-        for (let jndex = 0; jndex < array.length; jndex++) {
-            if (array[jndex] > array[jndex +1 ]) 
-                [array[jndex + 1],array[jndex]] = [array[jndex],array[jndex + 1]]          
+    for (let index = array.length; index > 0 ; index--) {
+        for (let jndex = 0; jndex <= index-1; jndex++) {
+            console.log(array,"||",array[jndex],array[jndex+1])
+            if (array[jndex] > array[jndex+1]) 
+                [array[jndex],array[jndex+1]] = [array[jndex+1],array[jndex]]          
         }
     }
     return array
 }
 
 
-console.log(bubbleSort([11,10,9,8,7,6,5,4,3,2,1,0]))
+bubbleSort([11,10,9,8,7,6,5,4,3,2,1,0])
