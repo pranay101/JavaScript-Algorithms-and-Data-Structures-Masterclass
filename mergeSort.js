bef3a0d8f9483eef1892c9082e7aa116da388df1
pranay101 <pranayprajapati101@gmail.com>
2021-08-15 23:03:05 +0530

    implemented merge sort

--- mergeSort.js
+++ mergeSort.js
@@ -1,38 +1,45 @@
 // ---------------------------------------Helper Function------------------------------------------------
-function merge(arr1,arr2)
-{
-    let arr_final= [];
-    var i =0, j=0;
-    while (i < arr1.length || j < arr2.length) 
-    {
-        if(i < arr1.length)
-		{
-        	if (arr1[i] < arr2[j]) 
-			{
-				arr_final.push(arr1[i])
-				i++
-			}
-		}
-		else
-		{
-			arr_final.push(arr2[j])
-			j++
-		}
-		if(j <= arr2.length)
-		{
-			if (arr1[i] > arr2[j])
-			{
-				arr_final.push(arr2[j])
-				j++
-			}
-		}
-	    else{
-		    arr_final.push(arr1[i])
-		    i++
-	    }
 
+function merge(arr1, arr2){
+    let results = [];
+    let i = 0;
+    let j = 0;
+    while(i < arr1.length && j < arr2.length){
+        if(arr2[j] > arr1[i]){
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j])
+            j++;
+        }
     }
-    return arr_final
+    while(i < arr1.length) {
+        results.push(arr1[i])
+        i++;
+    }
+    while(j < arr2.length) {
+        results.push(arr2[j])
+        j++;
+    }
+    return results;
 }
 
-console.log(merge([1,10,50],[2,14,99,100,123,13424,454355]))
+
+
+
+
+// ---------------------------------------Merge sort----------------------------------------------------
+function mergeSort(arr)
+{
+	if(arr.length <= 1) return arr;
+
+	let mid = Math.floor(arr.length/2);
+	let left = mergeSort(arr.slice(0,mid));
+	let right = mergeSort(arr.slice(mid));
+
+	return merge(left,right);
+	
+}
+
+
+console.log(mergeSort([42,14,9,0,1423,1,34,2,4,45,435,5]))
