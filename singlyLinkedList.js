653c979c55ebcd66e689d951654ef9ca157a7f38
pranay101 <pranayprajapati101@gmail.com>
2021-08-18 13:28:57 +0530

    Starting with singlyLinkedList

---
+++ singlyLinkedList.js
@@ -0,0 +1,25 @@
+// ---------------------------------------Introduction----------------------------------------------------
+// Linked list dont have any indexes.
+// connected via nodes with a next pointer
+// random access of elements is not allowed
+// need to keep track of head -> first_element, tail -> the_last_element and length -> length of the list
+
+
+
+
+
+
+
+// ---------------------------------------Base Code------------------------------------------------------
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null
+    }
+}
+
+
+
+
+// ---------------------------------------Helper Function------------------------------------------------
+// ---------------------------------------H
