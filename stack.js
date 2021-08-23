f538f86ce0440195ef96a92ff7cf4325a3c9b801
pranay101 <pranayprajapati101@gmail.com>
2021-08-23 13:46:34 +0530

    getting started with stack

---
+++ stack.js
@@ -0,0 +1,7 @@
+// ---------------------------------------Introduction----------------------------------------------------
+// stack follows LIFO i.e. last in first out
+// used in : Managin function invocation, redo-undo, Routing(history objects) is treated like stack
+
+
+
+// ---------------------------------------Base Code------------------------------------------------------
