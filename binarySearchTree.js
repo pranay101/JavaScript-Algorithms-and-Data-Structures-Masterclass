9fc2b07ff3d0c8319727bfcb5e20538e98d77cc1
pranay101 <pranayprajapati101@gmail.com>
2021-08-25 16:23:34 +0530

    wrote notes for BST

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,4 +1,32 @@
 // ---------------------------------------Introduction----------------------------------------------------
+// Objective:
+// Define what is tree?
+// Compare and Contrast trees and lists
+// Explin the diffrence between trees, binary tree and binary search tree?
+// Implemented opration on BST
+// 
+
+
+// Parts of a tree
+// root: top-most node
+// Child: A node directly connected to another node when moving away from the root 
+// parent: the converse notion of a child
+// Siblings: Group nodes with same parents
+// Leaf: node with no childern
+// Edge: The connection between two nodes
+
+// Uses:
+// HTML DOM
+// Network Routing
+// Abstract syntax tree
+// artificial intelligence(decision tree)
+// Folders in Operating system
+// Computer file system
+
+// How binary search tree works:
+// Every parent node has at most 2 nodes
+// Every node on the left of the parent node is always less than the parent.
+// Every node on the Rigth of the parent node is always greator than the parent.
 
 
 // ---------------------------------------Base Code------------------------------------------------------
