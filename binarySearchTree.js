e035ff77958d8bd2fc3cdbc56d08548c6b9210e6
pranay101 <pranayprajapati101@gmail.com>
2021-08-25 16:44:12 +0530

    done with bst classes

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -30,3 +30,18 @@
 
 
 // ---------------------------------------Base Code------------------------------------------------------
+class  Node{
+    constructor(value)
+    {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+class BinarySearchTree{
+    constructor() {
+        this.root = null
+    }
+}
+
+const tree = new BinarySearchTree();
