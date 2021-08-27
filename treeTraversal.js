f343ff65da79ed090f1ec212fcafaf9e9ba15482
pranay101 <pranayprajapati101@gmail.com>
2021-08-27 19:13:06 +0530

    Done with tree traversal added recap

--- treeTraversal.js
+++ treeTraversal.js
@@ -157,3 +157,15 @@ class BinarySearchTree{
 }
 
 const tree = new BinarySearchTree();
+
+
+
+
+
+
+// ---------------------------------------Recap--------------------------------------------------------
+
+// Trees are non-linear data structures that contain a root and child nodes
+// Binary Trees can have values of any type, but at most two children for each parent
+// Binary Search Trees are a more specific version of binary trees where every node to the left of a parent is less than it's value and every node to the right is greater
+// We can search through Trees using BFS and DFS
