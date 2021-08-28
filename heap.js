16827f5cb4d2050ca3aeb0fa0dc34ca257531369
pranay101 <pranayprajapati101@gmail.com>
2021-08-28 21:31:42 +0530

    Starting with binary heaps

---
+++ heap.js
@@ -0,0 +1,14 @@
+// ---------------------------------------Introduction----------------------------------------------------
+
+// OBJECTIVES
+// Define what a binary heap is
+// Compare and contrast min and max heaps
+// Implement basic methods on heaps
+// Understand where heaps are used in the real world and what other data structures can 
+// be constructed from heaps.
+
+
+// WHAT IS A BINARY HEAP?
+// Very similar to a binary search tree, but with some different rules!
+// In a MaxBinaryHeap, parent nodes are always larger than child nodes. 
+// In a MinBinaryHeap, parent nodes are always smaller than child nodes
