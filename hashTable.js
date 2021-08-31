f21017aaf8c5e8a8d8623278681b7cd6011b750f
pranay101 <pranayprajapati101@gmail.com>
2021-08-31 17:01:19 +0530

    Starting with hash tables

---
+++ hashTable.js
@@ -0,0 +1,12 @@
+// ---------------------------------------Introduction----------------------------------------------------
+
+// OBJECTIVES
+// Explain what a hash table is
+// Define what a hashing algorithm
+// Discuss what makes a good hashing algorithm
+// Understand how collisions occur in a hash table
+// Handle collisions using separate chaining or linear probing
+
+
+// ---------------------------------------Base Code------------------------------------------------------
+ 
