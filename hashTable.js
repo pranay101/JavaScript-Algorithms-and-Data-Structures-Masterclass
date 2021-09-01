d406890c4891a4ca72f17a26a39063d70efc59ac
pranay101 <pranayprajapati101@gmail.com>
2021-09-01 15:08:45 +0530

    Implemented set() in hashtable

--- hashTable.js
+++ hashTable.js
@@ -24,34 +24,46 @@
 
 
 // ---------------------------------------Base Code------------------------------------------------------
- class HashTable{
-     constructor(size=4) {
-         this.keyMap = new Array(size)
-     }
+class HashTable {
+    constructor(size = 4) {
+        this.keyMap = new Array(size)
+    }
 
-     _hash(key){
-         let total = 0
-         let WEIRD_PRIME = 31;
-         for (let i = 0; i < Math.min(key.length,100); i++){
-             let char  = key[i]
-             let value = char.charCodeAt(0)-96;
-             total  = (total * WEIRD_PRIME + value) % this.keyMap.length;
-         }
-         return total;
-     }
-     set(key,value){
-        let hash  = this._hash(key);
+    _hash(key) {
+        let total = 0
+        let WEIRD_PRIME = 31;
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i]
+            let value = char.charCodeAt(0) - 96;
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+        }
+        return total;
+    }
+    set(key, value) {
+        let hash = this._hash(key);
         if (!this.keyMap[hash]) {
             this.keyMap[hash] = ([]);
 
         }
-        this.keyMap[hash].push([key,value]);
+        this.keyMap[hash].push([key, value]);
         return this
-     }
- }
+    }
+    get(key) {
+        let index = this._hash(key);
+        if (this.keyMap[index]) {
+            for (let i = 0; i < this.keyMap[index].length; i++) {
+                if (this.keyMap[index][i][0] === key) {
+                    return this.keyMap[index][i];
+                }
+            };
+        }
+        return undefined;
+    }
+}
 
- let hashTable =  new HashTable;
- console.log(hashTable.set("hello",56))
- console.log(hashTable.set("pranay",36))
- console.log(hashTable.set("prajapati",46))
- console.log(hashTable.set("prajatbttpati",46))
+let hashTable = new HashTable;
+console.log(hashTable.set("hello", 56))
+console.log(hashTable.set("pranay", 36))
+console.log(hashTable.set("prajapati", 46))
+console.log(hashTable.set("prajatbttpati", 46))
+console.log(hashTable.get("hellco"))
