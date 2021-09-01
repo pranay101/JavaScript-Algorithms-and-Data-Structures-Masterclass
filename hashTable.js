70230021d765142174492b24075e8289cdf396a8
pranay101 <pranayprajapati101@gmail.com>
2021-09-01 14:54:01 +0530

    Implemented set() in hashtable

--- hashTable.js
+++ hashTable.js
@@ -7,6 +7,51 @@
 // Understand how collisions occur in a hash table
 // Handle collisions using separate chaining or linear probing
 
+// Dealing with Collision
+
+// Separate Chaining:
+// With separate chaining, at each index in our array we store values using a 
+// more sophisticated data structure (e.g. an array or a linked list).
+// This allows us to store multiple key-value pairs at the same index.
+
+// Linear Probing:
+// With linear probing, when we find a collision, we search through the array to 
+// find the next empty slot.
+
+// Unlike with separate chaining, this allows us to store a single key-value at each index.
+
+
+
 
 // ---------------------------------------Base Code------------------------------------------------------
- 
+ class HashTable{
+     constructor(size=4) {
+         this.keyMap = new Array(size)
+     }
+
+     _hash(key){
+         let total = 0
+         let WEIRD_PRIME = 31;
+         for (let i = 0; i < Math.min(key.length,100); i++){
+             let char  = key[i]
+             let value = char.charCodeAt(0)-96;
+             total  = (total * WEIRD_PRIME + value) % this.keyMap.length;
+         }
+         return total;
+     }
+     set(key,value){
+        let hash  = this._hash(key);
+        if (!this.keyMap[hash]) {
+            this.keyMap[hash] = ([]);
+
+        }
+        this.keyMap[hash].push([key,value]);
+        return this
+     }
+ }
+
+ let hashTable =  new HashTable;
+ console.log(hashTable.set("hello",56))
+ console.log(hashTable.set("pranay",36))
+ console.log(hashTable.set("prajapati",46))
+ console.log(hashTable.set("prajatbttpati",46))
